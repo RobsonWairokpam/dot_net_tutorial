55ee8594aaa1138811abfe8336eb1174cac58830
Robson Wairokpam <91126192+RobsonWairokpam@users.noreply.github.com>
2025-05-08 17:07:21 +0530

    token generate

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97e3c1db1c37764298fe3256454af6aada8f5f7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9751f3ab14afaf07fb0b158acd579d01d4dc233")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
